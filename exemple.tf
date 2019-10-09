966a234a497aadb9c8bc9dadb31b063fa53f9d31
Frédéric Benoit <frederic.benoit@greencom-networks.com>
2019-10-09 17:17:52 +0200

    now creating also an elastic ip

--- exemple.tf
+++ exemple.tf
@@ -2,8 +2,14 @@ provider "aws" {
   profile    = "default"
   region     = "us-east-1"
 }
+#an 'elastic' ip
+resource "aws_eip" "ip" {
+    vpc = true
+    instance = aws_instance.firstone.id
+}
 
-resource "aws_instance" "example" {
+#the ec2 instance itself
+resource "aws_instance" "firstone" {
   ami           = "ami-b374d5a5"
   instance_type = "t2.micro"
 }
