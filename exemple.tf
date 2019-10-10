93697c999130b0e1b37205db58cbe07459ad7f96
Frédéric Benoit <frederic.benoit@greencom-networks.com>
2019-10-10 17:10:00 +0200

    change region + add another instance + add s3 bucket

--- exemple.tf
+++ exemple.tf
@@ -1,15 +1,32 @@
 provider "aws" {
   profile    = "default"
-  region     = "us-east-1"
+  region     = "us-west-2"
 }
+
+#test the usage of a bucket
+resource "aws_s3_bucket" "firstone" {
+  # NOTE: S3 bucket names must be unique across _all_ AWS accounts, so
+  # this name must be changed before applying this example to avoid naming
+  # conflicts.
+  bucket = "terraform-mybucketthat-dont-exist-anywhere-else"
+  acl    = "private"
+}
+
 #an 'elastic' ip
 resource "aws_eip" "ip" {
     vpc = true
     instance = aws_instance.firstone.id
 }
 
-#the ec2 instance itself
+#the ec2 instance itself that depend to s3 bucket
 resource "aws_instance" "firstone" {
-  ami           = "ami-b374d5a5"
+  ami           = "ami-fa82739a"
+  instance_type = "t2.micro"
+  depends_on = [aws_s3_bucket.firstone]
+}
+
+#add another instance not relataed to s3 bucket
+resource "aws_instance" "secondone" {
+  ami           = "ami-fa82739a"
   instance_type = "t2.micro"
 }
